6f28cd186f4251d1dedcaa502e721d5b44ba1d24
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-11-15 13:05:13 +0100

    chore(modeler): add background color

--- src/builder/bpmnModelerBuilder.ts
+++ src/builder/bpmnModelerBuilder.ts
@@ -52,6 +52,7 @@ export class BpmnModelerBuilder {
               width: 100%;
               height: 100%;
               overflow: hidden;
+              background-color: white;
             }
 
             .buttons {
