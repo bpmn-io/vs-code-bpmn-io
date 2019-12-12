fd8edfa456fee3c3a26a66eb4fb5350984e949b6
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-12-12 09:54:28 +0100

    feat(extension): provide basic integration tests
    Closes #10

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,18 +1,73 @@
-import * as assert from 'assert';
+import { expect } from 'chai';
 import { before } from 'mocha';
+import * as path from 'path';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../extension';
+
+const COMMANDS = {
+	PREVIEW_CMD: "extension.bpmn-io.preview",
+	EDIT_CMD: "extension.bpmn-io.edit"
+};
+
+const TEST_FILE = path.join(__dirname, '..', 'fixtures', 'simple.bpmn');
 
 suite('Extension Test Suite', () => {
 	before(() => {
 		vscode.window.showInformationMessage('Start all tests.');
 	});
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
+	test('should start without error', async () => {
+
+		// when
+		const editor = await openFile(TEST_FILE);
+
+		// given
+		expect(editor).not.to.be.empty;
+	});
+
+
+	test('should open preview', async () => {
+
+		// given
+		const { document } = await openFile(TEST_FILE);
+
+		const { uri } = document;
+
+		// then
+		await vscode.commands.executeCommand(COMMANDS.PREVIEW_CMD, uri);
+		await sleep(500);
+	});
+
+
+	test('should open edit', async () => {
+
+		// given
+		const { document } = await openFile(TEST_FILE);
+
+		const { uri } = document;
+
+		// then
+		await vscode.commands.executeCommand(COMMANDS.EDIT_CMD, uri);
+		await sleep(500);
 	});
 });
+
+
+// helpers //////
+
+async function openFile(path: string): Promise<vscode.TextEditor> {
+	const uri = vscode.Uri.file(path);
+	const document = await vscode.workspace.openTextDocument(uri);
+	const editor = await vscode.window.showTextDocument(document);
+
+	// wait for editor to open
+	await sleep(500);
+
+	return editor;
+}
+
+function sleep(ms: number): Promise<void> {
+	return new Promise(resolve => {
+	  setTimeout(resolve, ms);
+	});
+}
