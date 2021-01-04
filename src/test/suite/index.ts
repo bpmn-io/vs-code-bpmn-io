9c7a1107184ceba4d406abf54da8c13986c0244f
Niklas Kiefer <niklas.kiefer@camunda.com>
2021-01-04 11:48:47 +0100

    test: update mocha#color API usage

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -3,11 +3,12 @@ import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
 export function run(): Promise<void> {
+
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
+    color: true
   });
-  mocha.useColors(true);
 
   const testsRoot = path.resolve(__dirname, '..');
 
