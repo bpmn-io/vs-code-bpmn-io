5863a4420d82cb0685659bcebeb885c0f10b0fd8
Nico Rehwaldt <git_nikku@nixis.de>
2023-08-18 14:14:38 +0200

    test: verify editor is open

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,6 +3,8 @@ import * as sinonChai from 'sinon-chai';
 
 import { describe, before, it } from 'mocha';
 
+import { expect } from 'chai';
+
 import * as path from 'node:path';
 
 import * as vscode from 'vscode';
@@ -26,19 +28,62 @@ describe('extension', function() {
   describe('basic', () => {
 
     it('should open file', async () => {
+
+      // when
       await vscode.commands.executeCommand('vscode.open', TEST_FILE);
+
+      // then
+      const extension = await getExtension(TEST_FILE);
+
+      expect(extension, 'editor open').to.exist;
     });
 
 
     it('should open as BPMN', async () => {
-      await vscode.commands.executeCommand('vscode.openWith', TEST_FILE, 'bpmn-io.bpmnEditor');
+
+      // when
+      await vscode.commands.executeCommand('vscode.open', TEST_FILE);
+
+      // then
+      const extension = await getExtension(TEST_FILE);
+
+      expect(extension, 'editor open').to.exist;
     });
 
 
     it('should create new BPMN file', async () => {
-      await vscode.commands.executeCommand('bpmn-io.bpmnEditor.new');
+
+      // when
+      const uri = await vscode.commands.executeCommand('bpmn-io.bpmnEditor.new');
+
+      // then
+      expect(uri, 'uri exists').to.exist;
+
+      const extension = await getExtension(TEST_FILE);
+
+      expect(extension, 'editor open').to.exist;
+    });
+
+
+    it('should close opened editor', async () => {
+
+      // given
+      await vscode.commands.executeCommand('vscode.open', TEST_FILE);
+
+      // when
+      await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+
+      // then
+      const extension = await getExtension(TEST_FILE);
+
+      expect(extension, 'editor closed').not.to.exist;
     });
 
   });
 
 });
+
+
+function getExtension(uri: vscode.Uri) {
+  return vscode.commands.executeCommand('bpmn-io.bpmnEditor.__state', uri);
+}
